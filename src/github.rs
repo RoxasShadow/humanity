0b7c4d14ccbd2bb0f09036d263b4e276206390f0
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-05-19 22:01:04 +0200

    *: split lib into multiple modules

---
+++ src/github.rs
@@ -0,0 +1,90 @@
+extern crate serde_json;
+
+use std::env;
+use std::io::prelude::*;
+
+use hyper::Client;
+use hyper::header::{Connection, UserAgent};
+
+use serde_json::error::Result as JSONResult;
+use serde::de::Deserialize;
+
+use humanity::{Humanity, HumanityBearer, User};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct GitHubContributor {
+    pub url: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GitHubOwner {
+    pub owner: GitHubContributor
+}
+
+pub struct GitHub;
+impl GitHub {
+   pub fn fetch_contributors(repo: &str) -> Vec<User> {
+        let url = format!("https://api.github.com/repos/{}/contributors", repo);
+        let github_contributors: Vec<GitHubContributor> = Self::http_request(url).unwrap();
+        github_contributors.into_iter().map(Self::fetch_user).collect()
+    }
+
+    pub fn fetch_owner(repo: &str) -> User {
+        let url = format!("https://api.github.com/repos/{}", repo);
+        let github_owner: GitHubOwner = Self::http_request(url).unwrap();
+        Self::fetch_user(github_owner.owner)
+    }
+
+    pub fn fetch_user(github_contributor: GitHubContributor) -> User {
+        Self::http_request(github_contributor.url).unwrap()
+    }
+
+    pub fn http_request<T>(url: String) -> JSONResult<T> where T: Deserialize {
+        let url = match env::var("GITHUB_ACCESS_TOKEN") {
+            Ok(access_token) => format!("{}?access_token={}", url, access_token),
+            Err(_)           => url
+        };
+
+        let mut body = String::new();
+        Client::new()
+            .get(&*url)
+            .header(Connection::close())
+            .header(UserAgent("Humanity".to_string()))
+            .send().unwrap()
+            .read_to_string(&mut body).unwrap();
+        serde_json::from_str(&*body)
+    }
+}
+
+impl HumanityBearer for GitHub {
+    fn generate(repo: &str) -> Humanity {
+        Humanity {
+            owner:        GitHub::fetch_owner(repo),
+            contributors: GitHub::fetch_contributors(repo)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::GitHub;
+    use humanity::Humanity;
+
+    #[test]
+    fn test_fetch_contributors() {
+        let contributors = GitHub::fetch_contributors("RoxasShadow/manageiq.org");
+        assert_eq!(contributors[0].clone().name.unwrap(), "Garrett LeSage".to_string());
+    }
+
+    #[test]
+    fn test_fetch_owner() {
+        let owner = GitHub::fetch_owner("RoxasShadow/manageiq.org");
+        assert_eq!(owner.name.unwrap(), "Giovanni Capuano".to_string());
+    }
+
+    #[test]
+    fn test_get_from_github() {
+       let humanity = Humanity::get_from::<GitHub>("RoxasShadow/manageiq.org");
+        assert!(!humanity.to_string().is_empty());
+    }
+}
---
+++ src/humanity.rs
@@ -0,0 +1,57 @@
+pub struct Humanity {
+    pub owner: User,
+    pub contributors: Vec<User>
+}
+
+impl<'a> Humanity {
+    pub fn get_from<B: HumanityBearer>(source: &'a str) -> Self {
+        B::generate(source)
+    }
+}
+
+impl ToString for Humanity {
+    fn to_string(&self) -> String {
+        let mut owner        = format!("/* OWNER */{}\n", self.owner.to_string());
+        let     contributors = format!("/* CONTRIBUTORS */{}", self.contributors.iter().map(|contributor| contributor.to_string()).collect::<String>());
+        owner.push_str(&*contributors);
+        owner
+    }
+}
+
+pub trait HumanityBearer {
+    fn generate(source: &str) -> Humanity;
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct User {
+    pub login: String,
+    pub name: Option<String>,
+    #[serde(rename="html_url")]
+    pub profile_url: String,
+    pub location: Option<String>,
+    #[serde(rename="blog")]
+    pub website: Option<String>
+}
+
+impl ToString for User {
+    fn to_string(&self) -> String {
+        let name = match self.name.to_owned() {
+            Some(name) => name,
+            _          => self.login.to_string()
+        };
+
+        let profile_url = format!("GitHub Profile: {}", self.profile_url);
+
+        let mut location = String::new();
+        if let Some(location_) = self.location.to_owned() {
+            location = format!("Location: {}", location_);
+        }
+
+        let mut website = String::new();
+        if let Some(website_) = self.website.to_owned() {
+            website = format!("Website: {}", website_);
+        }
+
+        [name, profile_url, location, website, " ".to_string()].iter().filter_map(|s| if s.is_empty() { None } else { Some(format!("\n  {}", s)) }).collect()
+    }
+}
